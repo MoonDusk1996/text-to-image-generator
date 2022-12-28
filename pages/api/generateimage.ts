a200a7f0ff4c4d147f253ff83f1f19d4ea05aaa8
Dusk <91294230+MoonDusk1996@users.noreply.github.com>
2022-12-28 19:04:28 -0400

    refac

--- pages/api/generateimage.ts
+++ pages/api/generateimage.ts
@@ -28,7 +28,7 @@ export default async function handler(
     const response = await openai.createImage({
       prompt: prompt,
       n: 1,
-      size: "1024x1024",
+      size: "512x512",
     });
     webhookClient.send({
       embeds: [
